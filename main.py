a181570fedbd8447ced82abe91538e9ca102df71
riverya4life <111198704+riverya4life@users.noreply.github.com>
2023-03-20 17:29:52 +0200

    Update main.py

--- main.py
+++ main.py
@@ -9,25 +9,26 @@ from asyncio import sleep
 
 bot = commands.Bot(command_prefix=PREFIX, intents=disnake.Intents.all(), case_insensitive=True)
 
-activity = [
-    disnake.Game(name = random.choice(gamesnames)),
-    disnake.Streaming(name = random.choice(streamingnames), url = random.choice(streamingurl)),
-    disnake.Activity(name = random.choice(watchingnames), type = disnake.ActivityType.watching),
-    disnake.Activity(name = random.choice(listeningnames), type = disnake.ActivityType.listening),
-    disnake.Activity(name = random.choice(competingnames), type = disnake.ActivityType.competing)
-]
+@tasks.loop(minutes=15)
+async def change_activity():
+    activity = [
+        disnake.Game(name = random.choice(gamesnames)),
+        disnake.Streaming(name = random.choice(streamingnames), url = random.choice(streamingurl)),
+        disnake.Activity(name = random.choice(watchingnames), type = disnake.ActivityType.watching),
+        disnake.Activity(name = random.choice(listeningnames), type = disnake.ActivityType.listening),
+        disnake.Activity(name = random.choice(competingnames), type = disnake.ActivityType.competing)
+    ]
+    await bot.change_presence(status = disnake.Status.idle, activity = random.choice(activity))
 
 @bot.event
 async def on_ready():
     print("Бот подключен")
+    change_activity.start()
     print()
     print(" - Информация о боте - ")
     print("Имя бота: {0.user}".format(bot))
     print(f"ID бота: {bot.user.id}")
-
-    while True:
-        await bot.change_presence(status = disnake.Status.idle, activity = random.choice(activity))
-        await sleep(60)
+    print(f"Пинг во время запуска: {round(bot.latency * 1000)} мс")
 
 
 @bot.event
